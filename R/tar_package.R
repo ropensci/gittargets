2ba7196d912c98e8cc8ac75baeca56df6fe7148f
wlandau <will.landau@gmail.com>
2021-11-10 15:38:18 -0500

    Add more files

---
+++ R/tar_package.R
@@ -0,0 +1,16 @@
+#' targets: Dynamic Function-Oriented Make-Like Declarative Pipelines for R
+#' @docType package
+#' @description Version control systems such as Git help researchers
+#'   track changes and history in data science projects,
+#'   and the `targets` package minimizes the computational cost of
+#'   keeping the latest results reproducible and up to date.
+#'   The `targit` package combines these two capabilities.
+#'   The `targets` data store becomes a version control repository
+#'   and stays synchronized with the Git repository of the source code.
+#'   Users can switch commits and branches without
+#'   invalidating the `targets` pipeline.
+#' @name targit-package
+#' @family help
+#' @importFrom gert git_add
+#' @importFrom targets tar_config_get
+NULL

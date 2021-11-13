e0a6b9514d70950bde845f9ff9087f2a3b4a01ca
wlandau <will.landau@gmail.com>
2021-11-13 18:36:11 -0500

    Use manifest trick

---
+++ R/zzz_manifest.R
@@ -0,0 +1,23 @@
+#' @title Write a manifest file.
+#' @export
+#' @description Not a user-side function.
+#' @details Writes a `manifest.json` file so the `pkgdown` site
+#'   can be deployed to RStudio Connect as Git-backed content.
+#' @return `NULL` (invisibly).
+#' @examples
+#' zzz_dpm_write_manifest()
+zzz_dpm_write_manifest <- function() {
+  # nocov start
+  pkgdown <- requireNamespace("pkgdown", quietly = TRUE) &&
+    eval(parse(text = "pkgdown::in_pkgdown()"))
+  if (pkgdown) {
+    withr::with_dir(
+      "..",
+      rsconnect::writeManifest(
+        appPrimaryDoc = "index.html",
+        contentCategory = "site"
+      )
+    )
+  }
+  # nocov end
+}
